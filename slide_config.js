59b34f76df6348ae39ffa404227fdbef9dd7cb26
Pedro Cambra <pedro.cambra@gmail.com>
2013-05-04 12:24:15 +0100

    Initial version of the slides

---
+++ slide_config.js
@@ -0,0 +1,40 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Plugin Haikus',
+    subtitle: 'Drupalcon Portland 2013',
+    //eventInfo: {
+    //  title: 'Drupalcon Portland',
+    //  date: '5/19/2013'
+    //},
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    //favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Steve Persch',
+    company: 'Senior Engineer<br>Palantir',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@stevector',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }, {
+    name: 'Pedro Cambra',
+    company: 'Drupal Commerce Developer<br>Commerce Guys',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }]
+};
+
